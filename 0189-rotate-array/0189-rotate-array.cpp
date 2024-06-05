0dfcd68c6be60321395fe3572df7b87e2132ed03
ManamritSingh <110588967+ManamritSingh@users.noreply.github.com>
2024-06-06 02:17:06 +0530

    Time: 25 ms (23.82%), Space: 27.3 MB (54.55%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -1,22 +1,10 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
-        int len = nums.size();
-        k = k % len; // To handle cases where k is greater than len
-        if (k == 0) return; // No need to rotate if k is 0
-
-        int count = 0; // Count of elements moved
-        for (int start = 0; count < len; start++) {
-            int current = start;
-            int prev = nums[start];
-            do {
-                int next = (current + k) % len;
-                int temp = nums[next];
-                nums[next] = prev;
-                prev = temp;
-                current = next;
-                count++;
-            } while (start != current);
-        }
+        int n = nums.size();
+        k=k%n;
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
     }
 };
