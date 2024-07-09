45f1a3002e3573f8d5369bbd393d74cb9063a3d1
ManamritSingh <110588967+ManamritSingh@users.noreply.github.com>
2024-07-10 00:36:55 +0530

    Time: 2 ms (55.88%), Space: 12.1 MB (5.06%) - LeetHub

---
+++ 0274-h-index/0274-h-index.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.rbegin(),citations.rend());
+        int l = citations.size();
+        for(int i=0 ; i<citations.size(); i++){
+            if(citations[i]>=i+1){
+                continue;
+            }
+            else{
+                return i;
+            }
+        }
+        return l;
+    }
+};
